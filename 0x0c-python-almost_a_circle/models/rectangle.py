7075982c05283d5c743ffcf18064a5338238c24c
R2ania <rania.ahmed2679@gmail.com>
2024-04-22 00:30:43 +0000

    rectangle

--- 0x0c-python-almost_a_circle/models/rectangle.py
+++ 0x0c-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,40 @@ class Rectangle(Base):
 		'''computes the area of rectangle'''
 		return self.width *  self.height
 
+
+	def display(self):
+		'''prints the rectangle'''
+		r = '\n' * self.y + \
+			(' ' * self.x + '#' * self.width + '\n') * self.height
+		print(r, end=' '
+
+
+	def __str__(self):
+		'''Returning string info of rectangle'''
+		return ' [{}] ({}) {} - {}/{}'.\ 
+			format(type(self).__name__, self.id, self.x, self.y,\
+			self.width, self.height)
+
+
+	def __update(self, id=None, width=None, height=None, x=None, y=None)
+		''' update instance attributes'''
+		if id is not None:
+			self.id = id
+		if width is not None:
+			self.width = width
+		if height is not None:
+			self.height = height
+		if x is not None:
+			self.x = x
+		if y is not None:
+			self.y = y
+
+
+	def Update(self, *args, **kwargs):
+		'''update instance attributes via args'''
+		# print(args, kwargs)
+		if args:
+			self.__update(*args)
+		elif kwargs:
+			self.__update(**kwargs)
+
